81ce76520411738f1e650b63900105e2b7a978df
Eli Goldner <etgld@posteo.us>
2025-09-25 07:57:38 -0700

    To run and to get other analytics via grep on the JSONL files

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import polars as pl
 import os
 import json  # if proves to be too slow or memory consuming look into https://github.com/ICRAR/ijson + https://github.com/lloyd/yajl
-from collections import Counter
 import argparse
+import random
 from itertools import chain, groupby
-from functools import reduce, partial, lru_cache
+from functools import partial, lru_cache
 from collections.abc import Iterable
-from operator import itemgetter
-from typing import Callable, Any
+from typing import Callable, cast
 import datetime
 import logging
 import pathlib
@@ -50,6 +49,10 @@ logging.basicConfig(
 )
 
 
+def __normalize(s: str) -> str:
+    return " ".join(s.strip().lower().split())
+
+
 # Keep it simple by avoiding time information for now
 # we can fold it back in if we need that degree of granularity
 @lru_cache
@@ -61,6 +64,7 @@ def parse_and_normalize_date(dt_str: str) -> datetime.date:
 @lru_cache
 def is_before(pt_earliest: str, note_date: str | None) -> bool:
     if note_date is None:
+        # If we don't know then rule it out
         return False
     return parse_and_normalize_date(pt_earliest) <= parse_and_normalize_date(note_date)
 
@@ -70,45 +74,146 @@ def mkdir(dir_name: str) -> None:
     _dir_name.mkdir(parents=True, exist_ok=True)
 
 
-def generic_sample(
+def save_jsonl(
+    output_dir: str, fn: str, note_json_list: list[dict[str, int | str]]
+) -> None:
+    mkdir(output_dir)
+    with open(os.path.join(output_dir, f"{fn}.jsonl"), mode="w") as f:
+        for note_json in note_json_list:
+            f.write(json.dumps(note_json))
+
+
+def word_count_filter(
     note_json_list: list[dict[str, int | str]],
     minimum_total_words: int = 500,
 ) -> list[dict[str, int | str]]:
     def has_minimum_total_words(
-        note_json: dict[str, int | str], miminum_total_words: int = minimum_total_words
+        note_json: dict[str, int | str], mininum_total_words: int = minimum_total_words
     ) -> bool:
-        return note_json.get("RPT_TEXT", "").split() >= miminum_total_words
+        return len(str(note_json.get("RPT_TEXT", "")).split()) >= mininum_total_words
+
+    return [
+        note_json for note_json in note_json_list if has_minimum_total_words(note_json)
+    ]
 
 
-def lmr_sampling(
-    note_json_list: list[dict[str, int | str]], sample_total: int = 250
+def lmr_provider_type_and_specialty_filter(
+    note_json_list: list[dict[str, int | str]],
 ) -> list[dict[str, int | str]]:
-    pass
+    relevant_provider_types = {
+        "attending",
+        "physician",
+        "nurse practitioner",
+        "physician assistant",
+        "resident",
+        "fellow",
+        "intern",
+    }
+    relevant_specialty_names = {
+        "oncology",
+        "radiation oncology",
+        "internal medicine",
+        "cardiology",
+        "hematology/oncology",
+        "surgery",
+        "thoracic",
+    }
+
+    def __has_relevant_provider_type(
+        note_json: dict[str, int | str],
+        relevant_provider_types: set[str] = relevant_provider_types,
+    ) -> bool:
+        return (
+            __normalize(cast(str, note_json.get("PROVIDER_TYPE", "")))
+            in relevant_provider_types
+        )
+
+    def __has_relevant_specialty_name(
+        note_json: dict[str, int | str],
+        relevant_specialty_names: set[str] = relevant_specialty_names,
+    ) -> bool:
+        return (
+            __normalize(cast(str, note_json.get("SPECIALTY_NAME", "")))
+            in relevant_specialty_names
+        )
+
+    def __has_criteria(
+        note_json: dict[str, int | str],
+        relevant_provider_types: set[str] = relevant_provider_types,
+        relevant_specialty_names: set[str] = relevant_specialty_names,
+    ) -> bool:
+        return __has_relevant_provider_type(
+            note_json, relevant_provider_types
+        ) and __has_relevant_specialty_name(note_json, relevant_specialty_names)
+
+    result = [note_json for note_json in note_json_list if __has_criteria(note_json)]
+    logger.info(
+        f"Total LMR notes before provider type and specialty name filtration: {len(note_json_list)} - after: {len(result)}"
+    )
+    return result
 
 
-def inpatient_and_progress_sampling(
-    note_json_list: list[dict[str, int | str]], sample_total: int = 250
+def inpatient_and_progress_provider_filter(
+    note_json_list: list[dict[str, int | str]],
 ) -> list[dict[str, int | str]]:
-    pass
+    relevant_provider_types = {
+        "physician",
+        "nurse practitioner",
+        "physician assistant",
+        "resident",
+        "fellow",
+    }
+
+    def __has_relevant_provider_type(
+        note_json: dict[str, int | str],
+        relevant_provider_types: set[str] = relevant_provider_types,
+    ) -> bool:
+        return (
+            __normalize(cast(str, note_json.get("PROVIDER_TYPE", "")))
+            in relevant_provider_types
+        )
+
+    result = [
+        note_json
+        for note_json in note_json_list
+        if __has_relevant_provider_type(note_json)
+    ]
+    logger.info(
+        f"Total inpatient+progress notes before provider type filtration: {len(note_json_list)} - after: {len(result)}"
+    )
+    return result
 
 
 def has_valid_mrn_and_date(
     mrn_to_earliest_date: dict[int, str], note_json: dict[str, str | int]
 ) -> bool:
     mrn = int(note_json["DFCI_MRN"])
+    if mrn not in mrn_to_earliest_date:
+        # invalid MRN
+        return False
     pt_earliest = mrn_to_earliest_date.get(mrn)
+    # Everything in the table has an earliest date
+    # so don't need to worry about misses
     note_date = note_json.get("EVENT_DATE")
+    # Absent dates handled here
     return is_before(pt_earliest, note_date)
 
 
 def get_valid_mrn_and_date_notes_from_csv(
-    mrn_to_earliest_date: dict[int, str], csv_path: str
+    mrn_to_earliest_date: dict[int, str],
+    csv_path: str,
+    debug_source: str | None = None,
 ) -> list[dict[str, str | int]]:
     local_valid_mrn_and_date = partial(has_valid_mrn_and_date, mrn_to_earliest_date)
     note_json_list = pl.read_csv(csv_path).to_dicts()
-    return [
+
+    results = [
         note_json for note_json in note_json_list if local_valid_mrn_and_date(note_json)
     ]
+    if debug_source is not None:
+        for node_json in results:
+            node_json["debug_source"] = debug_source
+    return results
 
 
 def get_valid_mrn_and_date_notes_from_json(
@@ -131,23 +236,24 @@ def get_valid_mrn_and_date_notes_from_json(
 def get_dir_to_valid_mrn_and_date_notes(
     mrn_to_earliest_date: dict[int, str], notes_dir: str
 ) -> dict[str, list[dict[str, str | int]]]:
-    def get_valid_mrn_and_date_notes(root: str, fn: str) -> list[dict[str, str | int]]:
-        if fn.lower().endswith("json"):
-            return get_valid_mrn_and_date_notes_from_json(
-                mrn_to_earliest_date, os.path.join(root, fn)
-            )
-        elif fn.lower().endswith("csv"):
-            return get_valid_mrn_and_date_notes_from_csv(
-                mrn_to_earliest_date, os.path.join(root, fn)
-            )
-        else:
-            ValueError(f"{os.path.join(root, fn)} has bad extension")
-            return []
+    def get_valid_mrn_and_date_notes(
+        root: str, files: list[str]
+    ) -> Iterable[dict[str, str | int]]:
+        for fn in files:
+            if fn.lower().endswith("json"):
+                yield from get_valid_mrn_and_date_notes_from_json(
+                    mrn_to_earliest_date, os.path.join(root, fn), os.path.basename(root)
+                )
+            elif fn.lower().endswith("csv"):
+                yield from get_valid_mrn_and_date_notes_from_csv(
+                    mrn_to_earliest_date, os.path.join(root, fn), os.path.basename(root)
+                )
+            else:
+                ValueError(f"{os.path.join(root, fn)} has bad extension")
 
     return {
-        os.path.basename(root): get_valid_mrn_and_date_notes(root, fn, root)
+        os.path.basename(root): list(get_valid_mrn_and_date_notes(root, files))
         for root, dirs, files in os.walk(notes_dir)
-        for fn in files
     }
 
 
@@ -159,13 +265,16 @@ def merge_by_named_predicates(
 ) -> dict[str, list[dict[str, str | int]]]:
     def grouped_by_predicate(
         predicate: Callable[[str], bool],
-    ) -> Iterable[str, list[dict[str, str | int]]]:
+    ) -> Iterable[tuple[str, list[dict[str, str | int]]]]:
         def key_fn(dir_and_notes: tuple[str, list[dict[str, str | int]]]) -> bool:
             dirname, _ = dir_and_notes
             return predicate(dirname)
 
-        return groupby(
-            sorted(dir_to_valid_mrn_and_date_notes.items(), key=key_fn), key=key_fn
+        return cast(
+            Iterable[tuple[str, list[dict[str, str | int]]]],
+            groupby(
+                sorted(dir_to_valid_mrn_and_date_notes.items(), key=key_fn), key=key_fn
+            ),
         )
 
     def re_grouped_notes(
@@ -184,7 +293,11 @@ def merge_by_named_predicates(
 
 
 def collect_notes_and_write_metrics(
-    pt_record_csv: str, notes_dir: str, output_dir: str, fields: list[str]
+    pt_record_csv: str,
+    notes_dir: str,
+    output_dir: str,
+    fields: list[str],
+    subsample_total: int = 250,
 ) -> None:
     pt_record_df = pl.read_csv(pt_record_csv)
     mrn_and_date_df = (
@@ -220,9 +333,36 @@ def collect_notes_and_write_metrics(
         "lmr": is_one_of(("lmr",)),
         "inpatient_and_progress": is_one_of(("inpatient", "progress")),
     }
+
+    name_to_initial_filter = {
+        "lmr": lmr_provider_type_and_specialty_filter,
+        "inpatient_and_progress": inpatient_and_progress_provider_filter,
+    }
     synthetic_category_to_notes = merge_by_named_predicates(
         dir_to_valid_mrn_and_date_notes, name_to_predicate
     )
+    for synthetic_category, notes in synthetic_category_to_notes.items():
+        initial_filter = name_to_initial_filter.get(synthetic_category, lambda s: s)
+        initial_filtered = initial_filter(notes)
+        save_jsonl(
+            os.path.join(output_dir, "before_word_count_filter"),
+            synthetic_category,
+            initial_filtered,
+        )
+        word_count_filtered = word_count_filter(initial_filtered)
+        logger.info(
+            f"{synthetic_category} total after word count filtration - {len(word_count_filtered)}"
+        )
+        save_jsonl(
+            os.path.join(output_dir, "after_word_count_filter"),
+            synthetic_category,
+            word_count_filtered,
+        )
+        save_jsonl(
+            os.path.join(output_dir, f"abbrev_{subsample_total}"),
+            synthetic_category,
+            random.sample(word_count_filtered, subsample_total),
+        )
 
 
 def main():
